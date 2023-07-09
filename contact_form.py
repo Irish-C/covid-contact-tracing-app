022d3fbd81e470df85ed5e4616bf7da373d02df5
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-09 23:08:45 +0800

    Add constructor and variables

--- contact_form.py
+++ contact_form.py
@@ -1,2 +1,21 @@
 # Contact Tracing Form class
 class ContactForm:
+    # Constructor
+    def __init__(self):
+        # Personal information
+        self.first_name = ""
+        self.last_name = ""
+        self.phone_number = ""
+        self.email_address = ""
+        self.address = ""
+        # Health information
+        self.symptoms = ""
+        self.symptom_onset = ""
+        self.tested_positive = False
+        self.test_date = ""
+        self.test_result = ""
+        # Contact information
+        self.close_contacts = []
+        self.travel_history = []
+        # Consent
+        self.consent = False

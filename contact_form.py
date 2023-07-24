e75083cd4037a6ffba0e9e9764581ce74f1bbe91
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-24 17:07:47 +0800

    Raise ValueError in PERSONAL INFORMATION
    Format and set the full name
    
    Remove non-digit characters from phone number and validate its length
    
    Validate email address format
    
    Set the address

--- contact_form.py
+++ contact_form.py
@@ -1,35 +1,28 @@
+import re
+
 # Contact Tracing Form class
 class ContactForm:
-    # Constructor
-    def __init__(self):
-        # Personal information
-        self.__first_name = ""
-        self.__last_name = ""
-        self.__phone_number = ""
-        self.__email_address = ""
-        self.__address = ""
-        # Health information
-        self.__symptoms = ""
-        self.__symptom_onset = ""
-        self.__tested_positive = False
-        self.__test_date = ""
-        self.__test_result = ""
-        # Contact information
-        self.__close_contacts = []  # List of close contacts in the last 14-days
-        self.__travel_history = []  # List of places visited
-        # Consent
-        self.__consent = False
-
     """ Methods """
-    # A method that sets personal information of the respondent
-    def set_personal_information(self, first_name, last_name, phone_number:int, email_address, address):
-        self.__first_name = first_name
-        self.__last_name = last_name
+    # PERSONAL INFORMATION
+    def set_personal_information(self, first_name, last_name, phone_number, email_address, address):
+        # Set fullname format
+        self.__fullname = '{} {}'.format(first_name.capitalize(), last_name.capitalize())
+        
+        # set and validate phone number
+        phone_number = re.sub(r'\D', '', phone_number)
+        if len(phone_number) != 11:
+            raise ValueError("Phone number must have 11 digits.")
         self.__phone_number = phone_number
+
+        # set and validate email address
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email_address):
+            raise ValueError("Invalid email address.")
         self.__email_address = email_address
+
+        # set address
         self.__address = address
 
-    # A method that sets health information of the respondent
+    # A method that sets health information
     def set_health_information(self, symptoms, symptom_onset, tested_positive, test_date="", test_result=""):
         self.__symptoms = symptoms
         self.__symptom_onset = symptom_onset

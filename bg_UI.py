00a9b6c22a76c3f07e707ddf620bc3983fca48ba
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-24 16:02:49 +0800

    Create bg UI window
    Initialize root window
    Get the current script directory
    Load the background image

--- bg_UI.py
+++ bg_UI.py
@@ -3,4 +3,15 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 class ContactFormGUI:
-    pass
+    # Initialize root window
+    def __init__(self, root):
+        self.root = root
+        self.root.title("COVID-19 Contact Tracing App")
+        self.root.geometry("800x620+{}+{}".format(int(self.root.winfo_screenwidth() / 2 - 400), 0))
+
+        # Get the current script directory
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+        # Load the background image
+        bg_image_path = os.path.join(script_dir, "image_widgets", "app_bg.png")
+        self.bg_image = Image.open(bg_image_path)

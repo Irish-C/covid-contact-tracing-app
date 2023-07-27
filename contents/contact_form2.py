0c4d31c028eee74ebce0aaebb15c6b7545daa489
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-27 15:18:10 +0800

    Copy format of contact_form for the background image

--- contents/contact_form2.py
+++ contents/contact_form2.py
@@ -1,13 +1,20 @@
 import tkinter as tk
-from contents.contact_form import ContactForm
+from tkinter import ttk, PhotoImage
+import tkcalendar
 
-class ContactForm2(ContactForm):
+class ContactForm:
     '''Contact Tracing Form Part 2'''
     def __init__(self, add_contact_window):
-        super().__init__(add_contact_window)
+        self.add_contact_window = add_contact_window
+        self.add_contact_window.title("Contact Tracing Form")
+        self.add_contact_window.geometry("900x620+{}+{}".format(int(self.add_contact_window.winfo_screenwidth() / 2 - 450), 0))
 
-        self.emergency_info()
+        self.image = PhotoImage(file="image_widgets/ui.png")    # Load the image using PhotoImage
 
-    def emergency_info(self):
-        pass
+        # Create a canvas inside the add_contact_window
+        self.canvas = tk.Canvas(self.add_contact_window, bd=0)
+        self.canvas.pack(fill="both", expand=True)
 
+        # Load and display the background image on the canvas
+        self.canvas.image = self.image  # Keep a reference to the image
+        self.canvas.create_image(0, 0, anchor='nw', image=self.image)

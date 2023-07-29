23e1d1c6034c4d76d22830097e819a84f0dfd66a
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-07-30 02:52:11 +0800

    Add additional comments for future reference

--- contents/buttons.py
+++ contents/buttons.py
@@ -2,9 +2,9 @@
 import tkinter as tk
 from tkinter import messagebox
 import re, subprocess, csv, os
-import shutil
 
 class Buttons:
+    '''Initialize constructor'''
     def __init__(self, contact_form_instance, contact_form2_instance):
         self.contact_form_instance = contact_form_instance
         self.contact_form2_instance = contact_form2_instance
@@ -12,6 +12,7 @@ class Buttons:
         self.user_data = None
 
     def next_window(self, event):
+        # Open the next window (contact_form2.py)
         subprocess.call(["python", "contact_form2.py"])
         self.previous_window_entries = None  # Reset previous window entries
         if self.contact_form_instance:
@@ -20,18 +21,19 @@ class Buttons:
             self.previous_window_entries = self.contact_form2_instance.get_entries2()
 
     def previous_window(self, event):
+        # Close the current window (contact_form2.py) and go back to the previous window (contact_form.py)
         self.contact_form2_instance.second_window.destroy()  # Close the current window
         subprocess.run(["python", "contact_form.py"]) 
 
-
     def submit_form(self):
         user_input = None
+        '''Collects data from ContactForm class.'''
         if self.contact_form_instance:
             user_input = self.contact_form_instance.get_entries()
         elif self.contact_form2_instance:
             user_input = self.contact_form2_instance.get_entries2()
         try:
-
+            # Put entries into dictionary
             dictlist = []
             for key, value in user_input.items():
                 entry_dict = {'key': key, 'value': value}
@@ -41,13 +43,12 @@ class Buttons:
             file_path = os.path.join(os.getcwd(), 'contact-tracing-data.csv')
             file_exists = os.path.isfile(file_path)
 
+            # Open csv file if exists
             with open('contact-tracing-data.csv', 'a', newline='') as file:
                 writer = csv.writer(file)
-
                 # If the file doesn't exist, write the header row
                 if not file_exists:
                     writer.writerow(list(user_input.keys()) + [''])
-
                 # Write the data row
                 writer.writerow(list(user_input.values()) + [''])
                 tk.messagebox.showinfo("Success", "Saved Succesfully!")
@@ -60,7 +61,11 @@ class Buttons:
 
             return False
 
+
     def submit_form2(self):
+        '''Collects data from ContactForm2 class.
+        Then combine with values from ContactForm class.
+        Lastly, Save to csv.'''
         user_input2 = None
 
         if self.contact_form_instance:
@@ -87,9 +92,11 @@ class Buttons:
             return False
 
     def save_data2(self, user_input):
+        # Save the user input data for later use
         self.user_data = user_input
 
     def save_data_to_csv(self, filename):
+        # Save the user data to a CSV file
         if self.user_data is not None:
             current_directory = os.getcwd()  # Get the current directory
             csv_file_path = os.path.join(current_directory, '..', 'contact-tracing-form')
